20c1622d50d3947119b37314e8599b041db30166
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-06-23 17:09:17 +0300

    Initial Commit

---
+++ MyBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyBrowser
+//
+//  Created by Oleksandr Solokha on 23.06.2020.
+//  Copyright © 2020 Oleksandr Solokha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
