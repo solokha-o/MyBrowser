8878c16d9a48e3e2cf56ce788703b0c9a7e8f469
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-06-26 13:57:09 +0300

    in MyBrowserViewController add progress view call setup and configure function configure goBackButtonAction configure goForwardButtonAction configure design view configure progressView configure didFinish navigation configure didStartProvisionalNavigation navigation setup estimated progress observer configure tool bar

--- MyBrowser/MyBrowserViewController.swift
+++ MyBrowser/MyBrowserViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 
 
 class MyBrowserViewController: UIViewController {
+    
     @IBOutlet weak var enterLinkTextField: UITextField! {
         didSet {
             enterLinkTextField.text = "https://"
@@ -18,25 +19,73 @@ class MyBrowserViewController: UIViewController {
     }
     @IBOutlet weak var loadLinkButtonOutlet: UIButton!
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var goBackButtonOutlet: UIBarButtonItem!
+    @IBOutlet weak var goForwardButtonOutlet: UIBarButtonItem!
+    @IBOutlet weak var toolBar: UIToolbar!
     // create instance LoadRequestModel to get Request from url
     let loadRequest = LoadRequestModel()
+    // add progress view
+    var progressView = UIProgressView()
+    // create NSKeyValueObservation instance
+    var estimatedProgressObserver: NSKeyValueObservation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //call setup and configure function
-        desingSetup()
+        designSetup()
+        setupEstimatedProgressObserver()
+        setToolBar()
+    }
+    // configure goBackButtonAction
+    @IBAction func goBackButtonAction(_ sender: UIBarButtonItem) {
+        webView.goBack()
+    }
+    //configure goForwardButtonAction
+    @IBAction func goForwardButtonAction(_ sender: UIBarButtonItem) {
+        webView.goForward()
     }
     //configure button action
     @IBAction func loadLinkButtonAction(_ sender: UIButton) {
         guard let link = enterLinkTextField.text else { return }
         loadRequest.loadLink(link: link, webView: webView)
     }
-    //configure desing view
-    func desingSetup() {
+    //configure design view
+    func designSetup() {
         let arrow = UIImage(systemName: "arrow.right.circle")
         loadLinkButtonOutlet.setImage(arrow, for: .normal)
-        navigationItem.title = "MyBrowser"
+        let chevronLeft = UIImage(systemName: "chevron.left")
+        goBackButtonOutlet.image = chevronLeft
+        let chevronRight = UIImage(systemName: "chevron.right")
+        goForwardButtonOutlet.image = chevronRight
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = webView.title
+        //configure progressView
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.progressTintColor = .red
+        let navigationBarBounds = self.navigationController?.navigationBar.bounds
+        progressView.frame = CGRect(x: 0, y: navigationBarBounds!.size.height - 2, width: navigationBarBounds!.size.width, height: 2)
+        progressView.autoresizingMask = [.flexibleWidth, .flexibleTopMargin]
+        progressView.progress = Float(webView.estimatedProgress)
+        navigationController?.navigationBar.addSubview(progressView)
+    }
+    // configure didFinish navigation
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        progressView.isHidden = true
+    }
+    //configure didStartProvisionalNavigation navigation
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        progressView.isHidden = false
+    }
+    //setup estimated progress observer
+    func setupEstimatedProgressObserver() {
+           estimatedProgressObserver = webView.observe(\.estimatedProgress, options: [.new]) { [weak self] webView, _ in
+               self?.progressView.progress = Float(webView.estimatedProgress)
+           }
+       }
+    //configure tool bar
+    func setToolBar() {
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        toolBar.setItems([goBackButtonOutlet, space, goForwardButtonOutlet], animated: true)
     }
 }
 
